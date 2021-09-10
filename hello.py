c5b96f86152e362f7a1ca3fac0510e0780e2eb3d
snsepton <sonja.septon@utah.edu>
2021-09-09 20:58:12 -0600

    py.files

---
+++ hello.py
@@ -0,0 +1,14 @@
+print('hello world')
+PI = 3.14
+print(PI)
+name =input('Enter your name:')
+print(name)
+print('What is your name?')
+name = input()
+print(name)
+x = input('Enter a number: ')
+print(type(x))
+x = int(input('Enter a number: '))
+print(type(x))
+x = 5
+print('The number is ' + str(x))
